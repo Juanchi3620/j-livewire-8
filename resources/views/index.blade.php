7b64eec245cb5d2b5a4226ce3c7faacecc2190b1
Juanchi <francovargasjuandario@gmail.com>
2021-12-22 19:47:40 -0500

    index terminado

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.web')
+
+@section('content')
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos cursos</h1>
+        <h2 class="text-xl text-gray-600">Fórmate Online como profesional en Técnologia</h2>
+        <h3 class="text-lg text-gray-600">70% de los graduados duplica sus ingresos</h3>
+    </div>
+
+    <livewire:course-list>
+@endsection
